adc10fda1910de7f33b6dc199ae6a4c38aa52943
U-PeterYordanov\Peter Yordanov <ppyordanov@yahoo.com>
2013-11-14 15:08:57 +0000

    profile pages template for students/tutors

---
+++ src/ProfileView.py
@@ -0,0 +1,9 @@
+# ProfileView.py
+# Peter Yordanov - 1103620y
+# 14/11/13
+
+# Subsection of PSD3 Team Project - Undergraduate Booking System.
+
+# Presents the user with account information after login
+# >> Tutor - sees classes that is tutoring, contact information (email, phone number, office, ..), (profile image)
+# >> Student - sees attendance record, classes taken , etc
